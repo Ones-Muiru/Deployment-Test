322f47cffeb927daa36087149cfb0a75f67a13c4
Onesphoro Muiru <onesphorokibunja@gmail.com>
2024-01-28 23:34:36 +0300

    audio file change

--- vstrial4.py
+++ vstrial4.py
@@ -92,8 +92,14 @@ def home_page(image_path):
     """, unsafe_allow_html=True)
 
 
+# def play_sound(sound_file):
+#     st.markdown(f'<audio src="{sound_file}" autoplay="autoplay" controls="controls"></audio>', unsafe_allow_html=True)
 def play_sound(sound_file):
-    st.markdown(f'<audio src="{sound_file}" autoplay="autoplay" controls="controls"></audio>', unsafe_allow_html=True)    
+    # Check if the predicted class is not c1
+    if categories[predicted_class] != 'c1':
+        # Play sound if the predicted class is not c1
+        st.markdown(f'<audio src="{sound_file}" autoplay="autoplay" controls="controls"></audio>', unsafe_allow_html=True)
+    
 
 def image_predictor_page(image_path):
     st.image(image_path, use_column_width=True)
